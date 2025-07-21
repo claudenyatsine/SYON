52ca9bfdc8c2d5a2c4f22c96ec8b085e4eb9ed10
Pelaiah Tadiwanashe Tapera Ngarande <iampelaiah.n@gmail.com>
2025-07-21 15:52:10 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FeaturesCarousel>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,7 +203,7 @@ export default function LandingPage() {
         </section>
 
         {/* Features Section */}
-        <section id="features" className="snap-section bg-secondary pt-[40vh]">
+        <section id="features" className="snap-section">
           <FeaturesCarousel />
         </section>
         
@@ -425,3 +425,5 @@ export default function LandingPage() {
     </div>
   );
 }
+
+    
